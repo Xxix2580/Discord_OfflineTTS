66cb3162ae2788e743d1e511819867bd7737dce1
Xxix2580 <jic4656@naver.com>
2022-12-07 15:30:25 +0900

    IP check command added.

--- pyttsbot.py
+++ pyttsbot.py
@@ -1,7 +1,10 @@
 import discord
 import pyttsx3
-from discord.ext import commands
+from discord.ext import commands, tasks
 import time
+from time import sleep
+from requests import get
+
 
 class Settings:
     def __init__(self, token, msgchannel='pytts', prefix='//'):
@@ -26,16 +29,34 @@ class TTS:
         self.engine = pyttsx3.init()
         self.engine.setProperty('rate', rate)
         self.engine.startLoop(False)
+        #List of TTSItem.
+        self.queue = []
+        self.lock = False
     
     def speak_to_file(self, msg, filename):
+        #print(msg)
         self.engine.save_to_file(msg, filename)
         self.engine.iterate()
-        time.sleep(len(msg) * (1/setting.rate) + 0.2)
+        #Just constant waiting time is more helpful.
+        time.sleep(1)
+        #time.sleep(len(msg) * (1/setting.rate) * 1.0 + 0.1)
+        print("Speak_to_file_return")
         return
     
     def stop(self):
         self.engine.endLoop()
         return
+
+class TTSItem:
+    def __init__(self, voice, channel, usrname, msg, ctx):
+        self.voice = voice
+        self.channel = channel
+        self.usrname = usrname
+        self.msg = msg
+        self.ctx = ctx
+        return
+    def __str__(self):
+        return "{}_{}".format(self.usrname, self.msg)
 
 
 # Token for PyOfflineTTS_Bot :
@@ -97,11 +118,13 @@ async def join(ctx):
 async def speak(ctx, msg : str):
     voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
     channel = discord.utils.get(ctx.guild.voice_channels, name=ctx.message.author.voice.channel.name)
-    if not voice:
+
+    tts.queue.append(TTSItem(voice,channel,ctx.message.author.id,msg, ctx))
+    '''if not voice:
         await channel.connect()
     voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
     tts.speak_to_file(msg, 'speech.mp3')
-    voice.play(discord.FFmpegPCMAudio('./speech.mp3'))
+    voice.play(discord.FFmpegPCMAudio('./speech.mp3'))'''
     #print('Spoken {}'.format(msg))
     return
 
@@ -165,6 +188,11 @@ async def set_voice_force(ctx, voice):
     await ctx.send('Now_Voice : {}'.format(str(now_voice)))
     return
 
+@client.command(pass_context=True)
+async def check_ip(ctx):
+    ip = get('https://api.ipify.org').text
+    await ctx.send('IP Addr : {}'.format(ip))
+    return
 
 @client.command(pass_context=True)
 async def umjunsik(ctx):
@@ -176,7 +204,40 @@ async def settings(ctx):
     await ctx.send(str(setting))
     return
 
+#LOOP
+@tasks.loop(seconds = 1) # repeat after every 10 seconds
+async def TTSLoop():
+    # work
+    if len(tts.queue) > 0 and tts.lock == False:
+        tsit:TTSItem = tts.queue[0]
+        #print(tsit.voice)
+        summon_channel = tsit.channel
+        if summon_channel is None:
+            return False
+        
+        voice = discord.utils.get(client.voice_clients, guild=tsit.ctx.guild)
+        if not (voice):
+            await summon_channel.connect()
+            voice = discord.utils.get(client.voice_clients, guild=tsit.ctx.guild)
+        tts.speak_to_file(tsit.msg, 'speech.mp3')
+
+        
+        #print(bool(voice))
+        try:
+            tts.lock = True
+            voice.play(discord.FFmpegPCMAudio('./speech.mp3'))
+            while voice.is_playing():
+                #print("is_playing")
+                sleep(0.1)
+            tts.lock = False
+        except:
+            print("Formal Discord Py Bug")
+        print("Loop End")
+        del tts.queue[0]
+    return
+
+
+TTSLoop.start()
 #issue-1 : Language.
 #-> https://stackoverflow.com/questions/65977155/change-pyttsx3-language
 client.run(setting.token)
-            
